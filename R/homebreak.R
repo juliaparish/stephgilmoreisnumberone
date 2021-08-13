c871fcdecf4788594bfe37cbb5b30c0333a651da
juliaparish <jparish@bren.ucsb.edu>
2021-08-13 12:01:54 -0700

    update package

--- R/homebreak.R
+++ R/homebreak.R
@@ -1,5 +1,7 @@
 # Create a function - should have at least 3 arguments in which one is a character, one is numeric
 
+library(devtools)
+
 #' Describes your home surf break and favorite surfer
 #'
 #' Contains a function, homebreak, that prints a statement about the users home break, how often they surf and fave surfer
@@ -12,9 +14,11 @@
 #' @export
 #'
 #' @examples
+
 home_break <- function(fave_break, visits, fave_surfer) {
   print(paste0("My homebreak is ", fave_break, ".", " I go there ", visits, " times a week.", " I want to surf there with ", fave_surfer))
 }
 
-home_break("Cliffs", 5, "Steph Gilmore")
+
+# To install package, devtools::install_github("juliaparish/stephgilmoreisnumberone")
 
